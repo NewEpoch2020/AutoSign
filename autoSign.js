25b66e41a3114f2ad1d6364d6fc494a87ae1ff83
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2021-08-19 18:25:43 +0800

    Add files via upload

---
+++ autoSign.js
@@ -0,0 +1,43 @@
+autoSign()
+
+async function autoSign(){
+    const puppeteer = require('puppeteer-extra');
+    const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+    puppeteer.use(StealthPlugin());
+    const axios = require('axios');
+    const config = require("./config");
+    const barkURL = "https://www.hidjnuiot78945huoridfngjkfnhf.xyz/J8AAL9oQYfySSKjgUSNQ9i/"
+    
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+
+    const {
+        cookies_Sehuatang,
+      } = config;
+
+     await Promise.all([ //没有顺序的概念
+        sehuatang(),
+        //bing(),
+        //google()
+    ]);
+ 
+   
+    async function sehuatang(){
+        console.log("Start sehuatang sign in...");
+        const page_sehuatang = await browser.newPage();
+        
+        await page_sehuatang.setCookie(...cookies_Sehuatang); //可变长度参数就是一个数组
+        
+        await page_sehuatang.goto('https://www.sehuatang.org/');
+
+        try {
+            await page_sehuatang.waitForSelector('#um > p:nth-child(2) > strong > a',{ timeout: 5000 })
+            console.log("Failed to sign in sehuatang!");
+        }catch (err){
+            console.log("Failed to sign in sehuatang!");
+            axios.post(barkURL + '[Sign] Failed to sign in sehuatang!');
+        }
+               
+    }   
+}
