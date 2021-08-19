885263ba7a9fa71e43c01c46dba50542919103fd
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2021-08-19 21:27:43 +0800

    Update autoSign.js

--- autoSign.js
+++ autoSign.js
@@ -5,35 +5,36 @@ async function autoSign(){
     const StealthPlugin = require('puppeteer-extra-plugin-stealth');
     puppeteer.use(StealthPlugin());
     const axios = require('axios');
-    const barkURL = process.env.BARK_URL;
     
-    let cookies_Sehuatang = eval(process.env.SEHUATANG_COOKIES);
+    const barkURL = process.env.BARK_URL;  
+    const cookies_Sehuatang = eval(process.env.SEHUATANG_COOKIES);
 
     const browser = await puppeteer.launch({
         headless: true //必须设置为无头模式
     });
 
-     await Promise.all([ //没有顺序的概念
+    await Promise.all([ //没有顺序的概念
         sehuatang(),
-        //bing(),
-        //google()
-    ]);
- 
+        //zodgame(),  //还没完成
+        //sketchupbar()
+        //pojie52()
+        //bilibili()
+    ])
+
+    await browser.close();
+
     async function sehuatang(){
-        console.log("Start sehuatang sign in...");
+        console.log("Start sign in sehuatang...");
         const page_sehuatang = await browser.newPage();
-        
-        await page_sehuatang.setCookie(...cookies_Sehuatang); //可变长度参数就是一个数组
-        
+        await page_sehuatang.setCookie(...cookies_Sehuatang); //可变长度参数就是一个数组   
         await page_sehuatang.goto('https://www.sehuatang.org/');
 
         try {
-            await page_sehuatang.waitForSelector('#um > p:nth-child(2) > strong > a',{ timeout: 5000 })
+            await page_sehuatang.waitForSelector('#um > p:nth-child(2) > strong > a')
             console.log("Succeed to sign in sehuatang!");
         }catch (err){
             console.log("Failed to sign in sehuatang!");
             axios.post(barkURL + '[Sign] Failed to sign in sehuatang!');
-        } 
-        await browser.close();
+        }
     }
 }
