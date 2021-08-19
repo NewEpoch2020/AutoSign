ed855d5ea37882676b6a0861a47ff62a3367c241
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2021-08-19 18:32:23 +0800

    Update autoSign.js

--- autoSign.js
+++ autoSign.js
@@ -5,7 +5,7 @@ async function autoSign(){
     const StealthPlugin = require('puppeteer-extra-plugin-stealth');
     puppeteer.use(StealthPlugin());
     const axios = require('axios');
-    const config = require("./config");
+    const config = require("./config/autoSign");
     const barkURL = "https://www.hidjnuiot78945huoridfngjkfnhf.xyz/J8AAL9oQYfySSKjgUSNQ9i/"
     
     const browser = await puppeteer.launch({
