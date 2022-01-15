cc29d68423bbb628918e3c88e5eb6a4dc5f6b819
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2022-01-15 14:46:26 +0800

    Update autoSign.js

--- autoSign.js
+++ autoSign.js
@@ -27,7 +27,7 @@ async function autoSign(){
     await Promise.all([ //没有顺序的概念
         //sketchupbar(),
         //pojie52(),
-        sehuatang(),
+        //sehuatang(),
         //zodgame(), 
         //zodgame_BUX(),
         bisi(),
