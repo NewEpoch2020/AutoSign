812ceaac7d0465ae230f2d48d83c9f97bc34e602
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2022-02-01 18:34:28 +0800

    Update autoSign.js

--- autoSign.js
+++ autoSign.js
@@ -26,7 +26,7 @@ async function autoSign(){
 
     await Promise.all([ //没有顺序的概念
         bisi(),  //上次失败因为页面元素选择器变化
-        sehuatang(), //上次失败因为增加了
+        sehuatang(), //上次失败因为增加了新验证
         zodgame(),  //上次失败因为页面重定向timeout设置太小
         zodgame_BUX(), //同上
         sketchupbar(), //疑似同上
