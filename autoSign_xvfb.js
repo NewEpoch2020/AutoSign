e465a56b93a5a35a6276253c8a9223f990f2df11
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2022-03-14 12:34:41 +0800

    Update autoSign_xvfb.js

--- autoSign_xvfb.js
+++ autoSign_xvfb.js
@@ -108,22 +108,27 @@ async function autoSign() {
     }
 
     async function zod_bux(page, sitename, cookies, url, timeout, selector) {
-        for (var i = 1; i <= 3; i++) {
-            await page.setCookie(...cookies);
-            await page.goto(url);
-            await page.waitForTimeout(timeout);                
-            if (await page.$(selector)) {
-                await page.waitForSelector(selector);
-                let time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-                console.log(time + " " + sitename + ": i = " + i + "，Succeed to find selector: " + selector);
-                await page.click(selector);
-                await page.waitForTimeout(timeout);
-            } else {
-                console.log(`No more selector found!`);
-                return;
-            }  
-       }
-
+        try {    
+            for (var i = 1; i <= 3; i++) {
+                await page.setCookie(...cookies);
+                await page.goto(url);
+                await page.waitForTimeout(timeout);                
+                if (await page.$(selector)) {
+                    await page.waitForSelector(selector);
+                    let time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+                    console.log(time + " " + sitename + ": i = " + i + "，Succeed to find selector: " + selector);
+                    await page.click(selector);
+                    await page.waitForTimeout(timeout);
+                } else {
+                    console.log(`No more selector found!`);
+                    return;
+                }  
+           }
+        } catch (err) {
+            console.log(`Failed to comment on ${sitename}!\n` + err);
+            axios.post(barkURL + `[Sign] Failed to sign in ${sitename}!`);
+            process.exit(1);
+        }
     }
 
     async function comment_acghh(page, sitename, cookies, url, timeout, ...selectors) {
