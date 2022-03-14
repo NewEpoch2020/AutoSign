c7ef7dc3e118a95650e118fa4f73efe2b3a0df5c
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2022-03-14 11:59:26 +0800

    Update autoSign_xvfb.js

--- autoSign_xvfb.js
+++ autoSign_xvfb.js
@@ -26,7 +26,7 @@ async function autoSign() {
 
     await Promise.all([ //没有顺序的概念
         zodgame(),
-        acghh(),
+        //acghh(),
     ]);
     await browser.close();
 
@@ -55,8 +55,8 @@ async function autoSign() {
         const selector_BUX = '#wp > div:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(1) > div:nth-child(4) tr:nth-child(3) > td:nth-child(6) > a';
         const page = await browser.newPage();
         await logAndGetCookies(page, url, 5000, cookies_zodgame, sitename, name_md5);
-        await sign_click(page, sitename, cookies_zodgame, url, 30000, selector1, selector2);
-        await sign_wait(page, sitename, cookies_zodgame, url_BUX, 40000, selector_BUX);
+        await sign_click(page, sitename, cookies_zodgame, url, 50000, selector1, selector2);
+        await sign_wait(page, sitename, cookies_zodgame, url_BUX, 50000, selector_BUX);
     }
 
     //--------------------------------------------------------------------------------------------------//
