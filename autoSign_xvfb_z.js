7c226008e0a3b0211cf86ddf904a64130d445bd9
东方未明 <75068628+NewEpoch2020@users.noreply.github.com>
2022-03-29 16:05:07 +0800

    Update autoSign_xvfb_z.js

--- autoSign_xvfb_z.js
+++ autoSign_xvfb_z.js
@@ -40,7 +40,7 @@ async function autoSign() {
         const selector4 = '#fwin_dialog_submit';
         const page = await browser.newPage();
         await logAndGetCookies(page, url, 15000, cookies_zodgame, sitename, name_md5);
-        await sign_click(page, sitename, cookies_zodgame, url, 40000, selector1, selector2);
+        //await sign_click(page, sitename, cookies_zodgame, url, 40000, selector1, selector2);
         await zod_bux(page, sitename, cookies_zodgame, url_BUX, 40000, selector3, selector4);   
     }
 
